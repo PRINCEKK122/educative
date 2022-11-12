9dcc0a07f4683770b97a68019abd4d61dc5c87d8
PRINCEKK122 <awuahkarikari12@gmail.com>
2022-11-12 01:52:13 +0000

    find first unique number in a list

---
+++ List/first_non_repeating_integer_in_list.py
@@ -0,0 +1,16 @@
+# Implement a function, find_first_unique(lst) that returns the first unique integer in the list.
+def find_first_unique(lst):
+    # time complexity - O(n^2) for the worse case when the non-duplicate is at the last element
+    # space complexity - O(1)
+    if len(lst) == 0:
+        return None
+
+    for i in range(len(lst)):
+        found_duplicate = False
+
+        for ele in lst[i+1:]:
+            if lst[i] == ele:
+                found_duplicate = True
+
+        if not found_duplicate:
+            return lst[i]
