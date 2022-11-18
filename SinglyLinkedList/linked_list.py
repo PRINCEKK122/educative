9d6a1a06e4901fdadeeefa83fcf157035eefa8b1
PRINCEKK122 <awuahkarikari12@gmail.com>
2022-11-18 01:07:57 +0000

    implemented combining two lists

--- SinglyLinkedList/linked_list.py
+++ SinglyLinkedList/linked_list.py
@@ -103,6 +103,20 @@ class SinglyLinkedList:
 
         return self
 
+    def union(self, linked_list2):
+        if self.is_empty():
+            return linked_list2
+        elif linked_lists2.is_empty():
+            return self
+
+        start_node = self.__head_node
+
+        while start_node.next is not None:
+            start_node = start_node.next
+
+        start_node.next = linked_lists2.get_head()
+        return self.remove_duplicates()
+
     def delete_by_value(self, value):
         if not self.is_empty():
             current_node = self.get_head()
@@ -138,7 +152,7 @@ class SinglyLinkedList:
             current_node = next_node
 
             self.__head_node = previous_node
-        return
+
 
     def is_empty(self):
         return self.get_head() is None
@@ -160,25 +174,39 @@ class SinglyLinkedList:
 
 
 if __name__ == "__main__":
-    linked_list = SinglyLinkedList()
-    linked_list.insert_at_head(3)
-    linked_list.insert_at_head(2)
-    linked_list.insert_at_head(1)
-    linked_list.insert_at_tail(4)
-    linked_list.insert_at_tail(5)
-    linked_list.insert_at(3, 10)
-    linked_list.insert_at_head(10)
-    linked_list.insert_at_head(10)
-    linked_list.insert_at_head(10)
-    linked_list.insert_at_head(10)
-    print(linked_list)
-    # linked_list.delete_at_head()
-    linked_list.delete_by_value(1)
-    # linked_list.reverse_linked_list()
-    print(linked_list)
-    print("Value found", linked_list.search(10))
-    print("Value found", linked_list.search(-1))
-    print(len(linked_list))
-    print("remove duplicates", linked_list.remove_duplicates())
+    # linked_list = SinglyLinkedList()
+    # linked_list.insert_at_head(3)
+    # linked_list.insert_at_head(2)
+    # linked_list.insert_at_head(1)
+    # linked_list.insert_at_tail(4)
+    # linked_list.insert_at_tail(5)
+    # linked_list.insert_at(3, 10)
+    # linked_list.insert_at_head(10)
+    # linked_list.insert_at_head(10)
+    # linked_list.insert_at_head(10)
+    # linked_list.insert_at_head(10)
+    # print(linked_list)
+    # # linked_list.delete_at_head()
+    # linked_list.delete_by_value(1)
+    # # linked_list.reverse_linked_list()
+    # print(linked_list)
+    # print("Value found", linked_list.search(10))
+    # print("Value found", linked_list.search(-1))
+    # print(len(linked_list))
+    # print("remove duplicates", linked_list.remove_duplicates())
 
-    print(linked_list)
+    # print(linked_list)
+
+    linked_lists1 = SinglyLinkedList()
+    linked_lists1.insert_at_tail(1)
+    linked_lists1.insert_at_tail(2)
+    linked_lists1.insert_at_tail(3)
+
+    linked_lists2 = SinglyLinkedList()
+    linked_lists2.insert_at_tail(4)
+    linked_lists2.insert_at_tail(5)
+    linked_lists2.insert_at_tail(6)
+    linked_lists2.insert_at_tail(4)
+
+    linked_lists1.union(linked_lists2)
+    print(linked_lists1)
